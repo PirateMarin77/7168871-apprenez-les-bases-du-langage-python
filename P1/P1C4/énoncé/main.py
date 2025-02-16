33a2a1722f279ac2916181bf029de281c440439d
PirateMarin77 <yeleennewman@gmail.com>
2025-02-16 17:28:50 +0000

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+nom = "Narcisse"
+age = 26
+print(f"Je m'appelle {nom} et j'ai {age} ans. ")
+age_actuel = int(age) + 10
+print(f"Je m'appelle {nom} et j'ai {age_actuel} ans maintenant.")
