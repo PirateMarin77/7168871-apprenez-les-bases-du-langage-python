84a830509dec6e95df0ad861253b2f7162c3b222
PirateMarin77 <yeleennewman@gmail.com>
2026-03-25 15:55:27 +0000

    Refactor main.py for number processing and average calculation
    Fix integer conversion and sum calculation for list elements.

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,16 @@
 # Ecrivez votre code ici !
+nombres = (" 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ")
+liste = nombres.split()
+for element in liste :
+  liste_entier = int (liste)
+  print(liste_entier)
+somme = sum(liste_entier)
+print(somme)
+somme = 0
+for x in liste_entier :
+  somme += x
+  print(somme)
+Moyenne = sum(liste_entier) // len(liste_entier)
+nb_sup = sum(1 for x in liste_entier if x > Moyenne )
+print('Moyenne : ', Moyenne)
+print('Eléments supérieurs :', nb_sup)
